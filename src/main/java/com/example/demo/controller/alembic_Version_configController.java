25e3066125003090ea762150b7f5974227bcc2ae
egarprasetya <egarprasetya20@gmail.com>
2019-11-28 09:45:22 +0700

    repair

--- src/main/java/com/example/demo/controller/alembic_Version_configController.java
+++ src/main/java/com/example/demo/controller/alembic_Version_configController.java
@@ -1,4 +1,5 @@
 package com.example.demo.controller;
+
 import com.example.demo.query.*;
 import com.example.demo.model.*;
 
@@ -24,65 +25,58 @@ import com.example.demo.connection.*;
 public class alembic_Version_configController {
 
 	stringkoneksi sk = new stringkoneksi();
-	AllQuery query_string= new AllQuery();
+	AllQuery query_string = new AllQuery();
 	AllDeleteQuery query_string_delete = new AllDeleteQuery();
-	PreparedStatement queryselect_alembic_version_config=null;
-	PreparedStatement querydelete_alembic_version_config=null;
+	PreparedStatement queryselect_alembic_version_config = null;
+	PreparedStatement querydelete_alembic_version_config = null;
 
-	
-
-	
 	@GetMapping("/GetAlembicVersionConfig")
-	public ArrayList<alembic_version_configModel> TampilAlembicVersionConfig() throws SQLException
-	{
+	public ArrayList<alembic_version_configModel> TampilAlembicVersionConfig() throws SQLException {
 		Connection Connection1 = DriverManager.getConnection(sk.Path_expr, sk.service_user, sk.service_password);
-	   queryselect_alembic_version_config=Connection1.prepareStatement(query_string.query_select_alembic_version_config);
-       // Statement Connected_Expression1 = Connection1.createStatement(); // Create_Expression (Connection1);
-      //  Connected_Expression1.setFetchSize(100);
+		queryselect_alembic_version_config = Connection1
+				.prepareStatement(query_string.query_select_alembic_version_config);
+		// Statement Connected_Expression1 = Connection1.createStatement(); //
+		// Create_Expression (Connection1);
+		// Connected_Expression1.setFetchSize(100);
 		ResultSet Cursor1 = queryselect_alembic_version_config.executeQuery();
-				//("select*from alembic_version_config");// Evaluate (Connected_Expression1)
-	      	 
-		
-		ArrayList<alembic_version_configModel> ListUser1 = new ArrayList<alembic_version_configModel>();
-	          while (Cursor1.next()) // while there_is_next_record_in (Cursor1)
-	          {       
-	        alembic_version_configModel ModelAlembic=new alembic_version_configModel();	
-	      
-	         ModelAlembic.version_num=Cursor1.getString(1);
-	         ListUser1.add(ModelAlembic);  
-	          
-	          }	    
-	          Connection1.close();
-	          return ListUser1;
-	         
-		      
+		// ("select*from alembic_version_config");// Evaluate (Connected_Expression1)
 
-}
-	
+		ArrayList<alembic_version_configModel> ListUser1 = new ArrayList<alembic_version_configModel>();
+		while (Cursor1.next()) // while there_is_next_record_in (Cursor1)
+		{
+			alembic_version_configModel ModelAlembic = new alembic_version_configModel();
+
+			ModelAlembic.version_num = Cursor1.getString(1);
+			ListUser1.add(ModelAlembic);
+
+		}
+		Connection1.close();
+		return ListUser1;
+
+	}
+
 	@DeleteMapping("/DeletePostAlembicVersionConfigParameter")
-	public int DeletePostAlembicVersionConfig(@RequestBody String version_num) throws SQLException
-	{
+	public int DeletePostAlembicVersionConfig(@RequestBody String version_num) throws SQLException {
 		Connection Connection1 = DriverManager.getConnection(sk.Path_expr, sk.service_user, sk.service_password);
-	      querydelete_alembic_version_config=Connection1.prepareStatement(query_string_delete.query_delete_alembic_version_config);
-		 querydelete_alembic_version_config.setString(1, version_num);   
+		querydelete_alembic_version_config = Connection1
+				.prepareStatement(query_string_delete.query_delete_alembic_version_config);
+		querydelete_alembic_version_config.setString(1, version_num);
 		int Cursor1 = querydelete_alembic_version_config.executeUpdate();// Evaluate (Connected_Expression1)
-		int a =0; 
+		int a = 0;
 		Connection1.close();
-		return a;    	         
-}
-	
+		return a;
+	}
+
 	@GetMapping("/DeleteGetAlembicVersionConfigParameter")
-	public int DeleteGetAlembicVersionConfig(@RequestBody String version_num) throws SQLException
-	{
+	public int DeleteGetAlembicVersionConfig(@RequestBody String version_num) throws SQLException {
 		Connection Connection1 = DriverManager.getConnection(sk.Path_expr, sk.service_user, sk.service_password);
-	      querydelete_alembic_version_config=Connection1.prepareStatement(query_string_delete.query_delete_alembic_version_config);
-		 querydelete_alembic_version_config.setString(1, version_num);   
+		querydelete_alembic_version_config = Connection1
+				.prepareStatement(query_string_delete.query_delete_alembic_version_config);
+		querydelete_alembic_version_config.setString(1, version_num);
 		int Cursor1 = querydelete_alembic_version_config.executeUpdate();// Evaluate (Connected_Expression1)
-		int a =0;  
+		int a = 0;
 		Connection1.close();
-		return a;    	         
-}
-	
-	
-	
+		return a;
+	}
+
 }
--- src/main/java/com/example/demo/controller/alembic_versionController.java
+++ src/main/java/com/example/demo/controller/alembic_versionController.java
@@ -22,78 +22,79 @@ import com.example.demo.query.*;
 @RestController
 @RequestMapping("/AlembicVersion")
 public class alembic_versionController {
-	PreparedStatement querydelete_alembic_version_config=null;
+	PreparedStatement querydelete_alembic_version_config = null;
 	AllDeleteQuery query_string_delete = new AllDeleteQuery();
 	stringkoneksi sk = new stringkoneksi();
-	AllQuery query_string= new AllQuery();
+	AllQuery query_string = new AllQuery();
 	query_select_parameter query_String_param = new query_select_parameter();
-	PreparedStatement queryselect_alembic_version=null;
+	PreparedStatement queryselect_alembic_version = null;
+
 	@GetMapping("/GetAlembicVersion")
-	public ArrayList<alembic_versionModel> TampilAlembicVersion() throws SQLException
-	{		
-		   Connection Connection1 = DriverManager.getConnection(sk.Path_expr, sk.service_user, sk.service_password);
-	        queryselect_alembic_version=Connection1.prepareStatement(query_string.query_select_alembic_version);
-	        ResultSet Cursor1 = queryselect_alembic_version.executeQuery();// Evaluate (Connected_Expression1)
-	      	  ArrayList<alembic_versionModel> ListUser1 = new ArrayList<alembic_versionModel>();
-	          while (Cursor1.next()) // while there_is_next_record_in (Cursor1)
-	          {       
-	        alembic_versionModel ModelAlembic=new alembic_versionModel();	
-	         ModelAlembic.version_num=Cursor1.getString(1);
-	         ListUser1.add(ModelAlembic);  
-	         
-	          }	          
-	          Connection1.close();
-	          return ListUser1;
+	public ArrayList<alembic_versionModel> TampilAlembicVersion() throws SQLException {
+		Connection Connection1 = DriverManager.getConnection(sk.Path_expr, sk.service_user, sk.service_password);
+		queryselect_alembic_version = Connection1.prepareStatement(query_string.query_select_alembic_version);
+		ResultSet Cursor1 = queryselect_alembic_version.executeQuery();// Evaluate (Connected_Expression1)
+		ArrayList<alembic_versionModel> ListUser1 = new ArrayList<alembic_versionModel>();
+		while (Cursor1.next()) // while there_is_next_record_in (Cursor1)
+		{
+			alembic_versionModel ModelAlembic = new alembic_versionModel();
+			ModelAlembic.version_num = Cursor1.getString(1);
+			ListUser1.add(ModelAlembic);
+			return ListUser1;
+		}
+		Connection1.close();
+		alembic_versionModel ModelAlembic = new alembic_versionModel();
+		ListUser1.add(ModelAlembic);
+		return ListUser1;
 
-}
-	
+	}
+
 	@PostMapping("/GetAlembicVersionParameter")
-	public ArrayList<alembic_versionModel> TampilAlembicVersionParameterSelect(@RequestBody String version_num) throws SQLException
-	{		
-		   Connection Connection1 = DriverManager.getConnection(sk.Path_expr, sk.service_user, sk.service_password);
-	        queryselect_alembic_version=Connection1.prepareStatement(query_String_param.query_select_alembic_version_param_version_num);
-	       
-	        queryselect_alembic_version.setString(1, version_num);
-	        ResultSet Cursor1 = queryselect_alembic_version.executeQuery();// Evaluate (Connected_Expression1)
-	      	  ArrayList<alembic_versionModel> ListUser1 = new ArrayList<alembic_versionModel>();
-	          while (Cursor1.next()) // while there_is_next_record_in (Cursor1)
-	          {       
-	        alembic_versionModel ModelAlembic=new alembic_versionModel();	
-	         ModelAlembic.version_num=Cursor1.getString(1);
-	         ListUser1.add(ModelAlembic);  
-	          return ListUser1;
-	          }	          
-	          Connection1.close();
-		        alembic_versionModel ModelAlembic=new alembic_versionModel();	
-		         ListUser1.add(ModelAlembic);  
-	          return ListUser1;
+	public ArrayList<alembic_versionModel> TampilAlembicVersionParameterSelect(@RequestBody String version_num)
+			throws SQLException {
+		Connection Connection1 = DriverManager.getConnection(sk.Path_expr, sk.service_user, sk.service_password);
+		queryselect_alembic_version = Connection1
+				.prepareStatement(query_String_param.query_select_alembic_version_param_version_num);
 
-}
-	
-	
+		queryselect_alembic_version.setString(1, version_num);
+		ResultSet Cursor1 = queryselect_alembic_version.executeQuery();// Evaluate (Connected_Expression1)
+		ArrayList<alembic_versionModel> ListUser1 = new ArrayList<alembic_versionModel>();
+		while (Cursor1.next()) // while there_is_next_record_in (Cursor1)
+		{
+			alembic_versionModel ModelAlembic = new alembic_versionModel();
+			ModelAlembic.version_num = Cursor1.getString(1);
+			ListUser1.add(ModelAlembic);
+			return ListUser1;
+		}
+		Connection1.close();
+		alembic_versionModel ModelAlembic = new alembic_versionModel();
+		ListUser1.add(ModelAlembic);
+		return ListUser1;
+
+	}
+
 	@PostMapping("/DeletePostAlembicVersionParameter")
-	public int DeletePostAlembicVersion(@RequestBody String version_num) throws SQLException
-	{
+	public int DeletePostAlembicVersion(@RequestBody String version_num) throws SQLException {
 		Connection Connection1 = DriverManager.getConnection(sk.Path_expr, sk.service_user, sk.service_password);
-	      querydelete_alembic_version_config=Connection1.prepareStatement(query_string_delete.query_delete_alembic_version);
-		 querydelete_alembic_version_config.setString(1, version_num);   
+		querydelete_alembic_version_config = Connection1
+				.prepareStatement(query_string_delete.query_delete_alembic_version);
+		querydelete_alembic_version_config.setString(1, version_num);
 		int Cursor1 = querydelete_alembic_version_config.executeUpdate();// Evaluate (Connected_Expression1)
-		int a =0; 
+		int a = 0;
 		Connection1.close();
-		return a;    	         
-}
-	
+		return a;
+	}
+
 	@GetMapping("/DeleteGetAlembicVersionParameter")
-	public int DeleteGetAlembicVersion(@RequestBody String version_num) throws SQLException
-	{
+	public int DeleteGetAlembicVersion(@RequestBody String version_num) throws SQLException {
 		Connection Connection1 = DriverManager.getConnection(sk.Path_expr, sk.service_user, sk.service_password);
-	      querydelete_alembic_version_config=Connection1.prepareStatement(query_string_delete.query_delete_alembic_version);
-		 querydelete_alembic_version_config.setString(1, version_num);   
+		querydelete_alembic_version_config = Connection1
+				.prepareStatement(query_string_delete.query_delete_alembic_version);
+		querydelete_alembic_version_config.setString(1, version_num);
 		int Cursor1 = querydelete_alembic_version_config.executeUpdate();// Evaluate (Connected_Expression1)
-		int a =0;  
+		int a = 0;
 		Connection1.close();
-		return a;    	         
-}
-	
-	
+		return a;
+	}
+
 }
