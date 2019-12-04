2de610ca912c871a8359175448b54c9409bdf0a1
Huda <Dummy.Workloads@gmail.com>
2019-12-04 17:11:33 +0700

    Login core

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,33 +21,55 @@ import com.example.demo.query.LoginQuery;
 import com.example.demo.query.query_select_parameter;
 
 @RestController
-@RequestMapping(produces="application/json",path="/login_user")
+@RequestMapping(produces = "application/json", path = "/login_user")
 public class LoginController
 {
 	LoginQuery loginquery = new LoginQuery();
 	stringkoneksi sk = new stringkoneksi();
-	
+
 	@PostMapping("/login")
-	public ArrayList<UserModel> postAuthsId(@RequestBody UserModel cfm) throws SQLException 
+	public ArrayList<UserModel> postAuthsId(@RequestBody UserModel cfm) throws SQLException
 	{
 		Connection Connection1 = DriverManager.getConnection(sk.Path_expr, sk.service_user, sk.service_password);
 		PreparedStatement a = Connection1.prepareStatement(loginquery.query_login);
-		
+
 		a.setString(1, cfm.username);
 		a.setString(2, cfm.password);
 		ResultSet Cursor1 = a.executeQuery();// Evaluate (Connected_Expression1)
 		ArrayList<UserModel> ListUser1 = new ArrayList<UserModel>();
-		while (Cursor1.next()) // while there_is_next_record_in (Cursor1)
-		{
-			UserModel Modeluser = new UserModel();
-			Modeluser.user_id = Cursor1.getString(1);
-			Modeluser.username = Cursor1.getString(2);
-			Modeluser.extensions_user = Cursor1.getString(3);
-			ListUser1.add(Modeluser);
-
-		}
+		Cursor1.next();
+		UserModel Modeluser = new UserModel();
+		Modeluser.user_id = Cursor1.getString(1);
+		Modeluser.username = Cursor1.getString(2);
+		Modeluser.extensions_user = Cursor1.getString(3);
+		ListUser1.add(Modeluser);
 		Connection1.close();
+		try
+		{
+			updateStatus(ListUser1.get(0).user_id, "1");
+		}
+		catch (Exception error)
+		{
+			error.printStackTrace();
+		}
+		//
 		return ListUser1;
+	}
+
+	public void updateStatus(String id, String status)
+	{
+		try
+		{
+			Connection Connection1 = DriverManager.getConnection(sk.Path_expr, sk.service_user, sk.service_password);
+			PreparedStatement a = Connection1.prepareStatement(loginquery.query_login2);
+
+			a.setString(1, status);
+			a.setString(2, id);
+			a.executeUpdate();
+		} catch (SQLException error)
+		{
+			error.printStackTrace();
+		}
 
 	}
 }
--- src/main/java/com/example/demo/query/LoginQuery.java
+++ src/main/java/com/example/demo/query/LoginQuery.java
@@ -3,11 +3,11 @@ package com.example.demo.query;
 public class LoginQuery
 {
 	public String query_login = "select users.user_id, users.username, ps_auths.realm "
-			+ "from users join ps_auths on users.extensions_user = ps_auths.id join status on users.status = status.status_id "
-			+ "where users.username = ? and users.password = ?"; 
+			+ "from users join ps_auths on users.extensions_user = ps_auths.id  "
+			+ "where users.username = ? and users.password = ? "; 
 	 
 		public String query_login1 = "select id_user, extension_user" + 
 				"from users"
 				+ "where username = ? and password = ?";
-	public String query_login2 = "UPDATE users SET status = ? WHERE id=?"; 
+	public String query_login2 = "UPDATE users SET status = ? WHERE user_id=?"; 
 }
