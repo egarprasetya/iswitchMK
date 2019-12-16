2d3b9774d7a113cb791a46147698bda227e21e47
egarprasetya <egarprasetya20@gmail.com>
2019-12-16 10:08:22 +0700

    war file

--- src/main/java/com/example/demo/Servicespring1Application.java
+++ src/main/java/com/example/demo/Servicespring1Application.java
@@ -2,10 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Servicespring1Application
+public class Servicespring1Application extends SpringBootServletInitializer
 {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder app)
+	{
+		return app.sources(Servicespring1Application.class);
+	}
 
 	public static void main(String[] args)
 	{
